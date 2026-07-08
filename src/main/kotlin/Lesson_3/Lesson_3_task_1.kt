277cd508acb186c90d555e33cc206fbd617697e6
Савин А <savin.alexey78@gmail.com>
2026-07-08 21:36:09 +0300

    KS-3-1 hello for day and evening

---
+++ src/main/kotlin/Lesson_3/Lesson_3_task_1.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson_5
+
+fun main() {
+
+    val userName = "Alexey"
+    val helloDay = "Good day"
+    val helloEvening = "Good evening"
+    var hello = helloDay + ", " + userName
+    println(hello)
+    hello = helloEvening + ", " + userName
+    println(hello)
+
+
+}
