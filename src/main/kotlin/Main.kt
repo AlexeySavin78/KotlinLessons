de28c38f040363afa8af8ee5f1336b425a458e2a
Савин А <savin.alexey78@gmail.com>
2026-07-11 23:11:32 +0300

    KS-3-1 hello for day and evening

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,14 @@ package org.example
 
 fun main() {
 
-    val s = readln(); println(s)
+    val name = "Alexey"
+    val hello = "Hello"
+
+    var helloDay = "$hello, $name, good day"
+    println(helloDay)
+
+    helloDay = "$hello, $name, good evening"
+    println(helloDay)
 
 
 }
