a6d9d6eb5d73d0576dc1c86691d4c170325203d6
Савин А <savin.alexey78@gmail.com>
2026-07-12 21:27:24 +0300

    KS-3-2 change of last name

---
+++ src/main/kotlin/ks_3_2/ks_3_2.kt
@@ -0,0 +1,13 @@
+package org.example.ks_3_2
+
+fun main() {
+
+    val firstName = "Татьяна"
+    val secondName = "Андреева"
+    val patronymic = "Сергеевна"
+    val newSecondName = "Сидорова"
+    val age = 22
+    println("$secondName $firstName $patronymic Возраст ${age-2}")
+    println("$newSecondName $firstName $patronymic Возраст $age")
+
+}
