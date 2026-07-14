8eddac0a69c2c18bd83cde53585210d8f8d47ba7
Савин А <savin.alexey78@gmail.com>
2026-07-14 16:50:03 +0300

    KS-3-3 multiplication table 2

--- src/main/kotlin/KS_3_3/ks_3_3.kt
+++ src/main/kotlin/KS_3_3/ks_3_3.kt
@@ -2,18 +2,13 @@ package org.example.KS_3_3
 
 fun main() {
 
-    var number = 7
-    println("$number * 1 = ${number * 1} $number * 2 = ${number * 2} " +
-            "$number * 3 = ${number * 3} $number * 4 = ${number * 4} " +
-            "$number * 5 = ${number * 5} $number * 6 = ${number * 6} " +
-            "$number * 7 = ${number * 7} $number * 8 = ${number * 8} " +
-            "$number * 9 = ${number * 9}")
-    number = 8
-    println("$number * 1 = ${number * 1} $number * 2 = ${number * 2} " +
-            "$number * 3 = ${number * 3} $number * 4 = ${number * 4} " +
-            "$number * 5 = ${number * 5} $number * 6 = ${number * 6} " +
-            "$number * 7 = ${number * 7} $number * 8 = ${number * 8} " +
-            "$number * 9 = ${number * 9}")
+    val number = 7
+    println("$number * 1 = ${number * 1} \n$number * 2 = ${number * 2} " +
+            "\n$number * 3 = ${number * 3} \n$number * 4 = ${number * 4} " +
+            "\n$number * 5 = ${number * 5} \n$number * 6 = ${number * 6} " +
+            "\n$number * 7 = ${number * 7} \n$number * 8 = ${number * 8} " +
+            "\n$number * 9 = ${number * 9}")
+
 
 
 
