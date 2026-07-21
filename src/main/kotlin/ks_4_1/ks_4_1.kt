3215344121be59110df369712722f9a5df427fc8
Савин А <savin.alexey78@gmail.com>
2026-07-21 17:37:54 +0300

    KS-4-1 Available tables

--- src/main/kotlin/ks_4_1/ks_4_1.kt
+++ src/main/kotlin/ks_4_1/ks_4_1.kt
@@ -4,8 +4,8 @@ fun main() {
 
     val bookedToday = 13
     val bookedTomorrow = 9
-    val isAvailableToday: Boolean = bookedToday < TOTAL_TABLES
-    val isAvailableTomorrow: Boolean = bookedTomorrow < TOTAL_TABLES
+    val isAvailableToday = bookedToday < TOTAL_TABLES
+    val isAvailableTomorrow = bookedTomorrow < TOTAL_TABLES
     println(
         "Доступность столиков на сегодня: $isAvailableToday " +
                 "\nДоступность столиков на завтра: $isAvailableTomorrow"
