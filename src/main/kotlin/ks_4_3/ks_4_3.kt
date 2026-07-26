840156f86ef3d97e6162dbdccd2f335b6d51d79b
Савин А <savin.alexey78@gmail.com>
2026-07-26 22:41:55 +0300

    KS-4-3 Plant monitoring 2

--- src/main/kotlin/ks_4_3/ks_4_3.kt
+++ src/main/kotlin/ks_4_3/ks_4_3.kt
@@ -4,10 +4,12 @@ fun main() {
 
     val isWeatherToday = true
     val isOpenAwning = true
-    val plantMonitoring = isWeatherToday && isOpenAwning && HUMIDITY_THRESHOLD == 20 && TIME_OF_YEAR != "winter"
+    val isHumidity = 20
+    val isTimeOfYear = "зима"
+    val plantMonitoring = isWeatherToday && isOpenAwning && HUMIDITY_THRESHOLD == isHumidity && TIME_OF_YEAR != isTimeOfYear
     println("Благоприятные ли условия сейчас для роста бобовых? $plantMonitoring")
 
 
 }
 const val HUMIDITY_THRESHOLD = 20
-const val TIME_OF_YEAR = "winter"
+const val TIME_OF_YEAR = "зима"
