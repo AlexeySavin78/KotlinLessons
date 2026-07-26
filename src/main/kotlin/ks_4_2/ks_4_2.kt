df511357e4b9a655dec247c4da4036d486994d60
Савин А <savin.alexey78@gmail.com>
2026-07-26 19:22:09 +0300

    KS-4-2 Category 5

--- src/main/kotlin/ks_4_2/ks_4_2.kt
+++ src/main/kotlin/ks_4_2/ks_4_2.kt
@@ -6,19 +6,22 @@ fun main() {
     val volumeFirstCargo = 80
     val weightSecondCargo = 50
     val volumeSecondCargo = 100
-    val average = weightFirstCargo > WEIGHT_MINIMAL && weightFirstCargo <= WEIGHT_MAXIMUM
-            && volumeFirstCargo < VOLUME_MAXIMUM
-    println(average)
-    val average2 = weightSecondCargo > WEIGHT_MINIMAL && weightSecondCargo <= WEIGHT_MAXIMUM
-            && volumeSecondCargo < VOLUME_MAXIMUM
-    println(average2)
-    println("Груз весом $weightFirstCargo и объёмом $volumeFirstCargo соответствует категории 'average'")
-    println("Груз весом $weightSecondCargo и объёмом $volumeSecondCargo соответствует категории 'average'")
-
-
+    println(
+        "Груз весом 20 и объёмом 80 соответствует категории 'average' ${
+            weightFirstCargo > WEIGHT_MINIMAL && weightFirstCargo <= WEIGHT_MAXIMUM
+                    && volumeFirstCargo < VOLUME_MAXIMUM
+        }"
+    )
+    println(
+        "Груз весом 50 и объёмом 100 соответствует категории 'average' ${
+            weightSecondCargo > WEIGHT_MINIMAL && weightSecondCargo <= WEIGHT_MAXIMUM
+                    && volumeSecondCargo < VOLUME_MAXIMUM
+        }"
+    )
 
 
 }
+
 const val WEIGHT_MINIMAL = 35
 const val WEIGHT_MAXIMUM = 100
 const val VOLUME_MAXIMUM = 100
