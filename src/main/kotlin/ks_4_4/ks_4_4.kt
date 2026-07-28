906b93b40e18ad9a5ba8f07284b8a8841957c0e9
Савин А <savin.alexey78@gmail.com>
2026-07-28 21:28:05 +0300

    KS-4-4 Plant inversion

---
+++ src/main/kotlin/ks_4_4/ks_4_4.kt
@@ -0,0 +1,13 @@
+package org.example.ks_4_4
+
+fun main() {
+
+    val isExerciseForHands = true
+    val isExerciseForLegs = false
+    val isExerciseForBack = false
+    val isExerciseForAbs = true
+    println("Упражнение для рук:    $isExerciseForHands \nУпражнение для ног:    $isExerciseForLegs " +
+            "\nУпражнение для спины:  $isExerciseForBack \nУпражнение для пресса: $isExerciseForAbs")
+
+
+}
