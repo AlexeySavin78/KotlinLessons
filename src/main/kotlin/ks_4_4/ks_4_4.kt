3d891b97897ecfac870d1db4b6a5505c6d029552
Савин А <savin.alexey78@gmail.com>
2026-07-29 22:08:23 +0300

    KS-4-4 inversion 5

--- src/main/kotlin/ks_4_4/ks_4_4.kt
+++ src/main/kotlin/ks_4_4/ks_4_4.kt
@@ -3,14 +3,14 @@ package org.example.ks_4_4
 fun main() {
 
     var isTrainingDay = 1
-    val isHands = true
-    val isLegs = false
-    val isBack = false
-    val isAbs = true
+    val isHands = 1
+    val isLegs = 2
+    val isBack = 2
+    val isAbs = 1
 
     println(
-        "Упражнения для рук:     $isHands \nУпражнения для ног:     $isLegs " +
-                "\nУпражнения для спины:   $isBack \nУпражнения для пресса:  $isAbs"
+        "Упражнения для рук:     ${isTrainingDay == isHands} \nУпражнения для ног:     ${isTrainingDay == isLegs} " +
+                "\nУпражнения для спины:   ${isTrainingDay == isBack} \nУпражнения для пресса:  ${isTrainingDay == isAbs}"
     )
 
 
