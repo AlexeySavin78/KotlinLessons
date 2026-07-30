741b36de1d3b8f98f772356812b16212449e6e36
Савин А <savin.alexey78@gmail.com>
2026-07-30 22:35:36 +0300

    KS-4-4 inversion 8

--- src/main/kotlin/ks_4_4/ks_4_4.kt
+++ src/main/kotlin/ks_4_4/ks_4_4.kt
@@ -8,24 +8,7 @@ fun main() {
       val isabs = false
       val islegs = !ishands
       val isback = !isabs
-//      val groupHandsABS = ishands && isabs
-//      val isGroupLegsBack = islegs && isback
 
     println(
         "Упражнения для рук:    ${trainingDay == ishands} \nУпражнения для ног:    ${trainingDay == islegs} \nУпражнения для спины:  ${trainingDay == isback} \nУпражнения для пресса: ${trainingDay == isabs}")
-//    val day = 5 // Можно менять на 1, 2, 3... для проверки разных дней
-
-    // Если день нечётный (1, 3, 5...) — тренируем руки и пресс
-    // Если день чётный (2, 4, 6...) — тренируем ноги и спину
-//    val forArms = day % 2 != 0
-//    val forLegs = !forArms
-//    val forBack = !forArms
-//    val forAbs = forArms
-//
-//    println(
-//        "Упражнения для рук: $forArms\n" +
-//                "Упражнения для ног: $forLegs\n" +
-//                "Упражнения для спины: $forBack\n" +
-//                "Упражнения для пресса: $forAbs"
-//    )
 }
