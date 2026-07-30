29999386306456c367c61ee5cea083b5be0034f4
Савин А <savin.alexey78@gmail.com>
2026-07-30 22:30:05 +0300

    KS-4-4 inversion 8

--- src/main/kotlin/ks_4_4/ks_4_4.kt
+++ src/main/kotlin/ks_4_4/ks_4_4.kt
@@ -2,29 +2,30 @@ package org.example.ks_4_4
 
 fun main() {
 
-//    var trainingDay = 5
-//    val groupHandsABS =
-//    val isGroupLegsBack = false
-//    val hands = "Упражнения для рук:"
-//    val Legs = "Упражнения для ног:"
-//    val Back = "Упражнения для спины:"
-//    val ABS = "Упражнения для спины:"
-//
-//    println(
-//        "${trainingDay}$Hands    ${isTrainingDay == isHands} \nУпражнения для ног:    ${isTrainingDay == isLegs} \nУпражнения для спины:  ${isTrainingDay == isBack} \nУпражнения для пресса: ${isTrainingDay == isAbs} ${isTrainingDay + 1}")
-    val day = 5 // Можно менять на 1, 2, 3... для проверки разных дней
+      val day = 5
+      val trainingDay = day % 2 == 0
+      val ishands = false
+      val isabs = false
+      val islegs = !ishands
+      val isback = !isabs
+//      val groupHandsABS = ishands && isabs
+//      val isGroupLegsBack = islegs && isback
+
+    println(
+        "Упражнения для рук:    ${trainingDay == ishands} \nУпражнения для ног:    ${trainingDay == islegs} \nУпражнения для спины:  ${trainingDay == isback} \nУпражнения для пресса: ${trainingDay == isabs}")
+//    val day = 5 // Можно менять на 1, 2, 3... для проверки разных дней
 
     // Если день нечётный (1, 3, 5...) — тренируем руки и пресс
     // Если день чётный (2, 4, 6...) — тренируем ноги и спину
-    val forArms = day % 2 != 0
-    val forLegs = !forArms
-    val forBack = !forArms
-    val forAbs = forArms
-
-    println(
-        "Упражнения для рук: $forArms\n" +
-                "Упражнения для ног: $forLegs\n" +
-                "Упражнения для спины: $forBack\n" +
-                "Упражнения для пресса: $forAbs"
-    )
+//    val forArms = day % 2 != 0
+//    val forLegs = !forArms
+//    val forBack = !forArms
+//    val forAbs = forArms
+//
+//    println(
+//        "Упражнения для рук: $forArms\n" +
+//                "Упражнения для ног: $forLegs\n" +
+//                "Упражнения для спины: $forBack\n" +
+//                "Упражнения для пресса: $forAbs"
+//    )
 }
