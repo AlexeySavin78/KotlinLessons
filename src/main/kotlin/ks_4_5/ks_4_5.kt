73c7a50056b16d0716890d25bf7ef42a21bd9a7c
Савин А <savin.alexey78@gmail.com>
2026-08-03 22:28:59 +0300

    KS-4-5 can departure 4

--- src/main/kotlin/ks_4_5/ks_4_5.kt
+++ src/main/kotlin/ks_4_5/ks_4_5.kt
@@ -3,8 +3,7 @@ package org.example.ks_4_5
 fun main() {
 
     val hull = readln().toBoolean()
-    val isHullIntact = hull == true
-    val isHullIntact1 = hull == false
+    val isHullIntact = hull == false
     val crew = readln().toInt()
     val hasEnoughCrew = crew in 55..70
     val hasEnoughCrew1 = crew == 70
@@ -13,10 +12,9 @@ fun main() {
     val isBoxProvisions1 = provisions >= 50
     val weather = readln().toBoolean()
     val isWeatherSafe = weather == true
-    val isWeatherSafe1 = weather == false
 
-    val canDeparture = (isHullIntact && hasEnoughCrew && isBoxProvisions && (isWeatherSafe || isWeatherSafe1))
-            || (isHullIntact1 && hasEnoughCrew1 && isBoxProvisions1 && isWeatherSafe)
+    val canDeparture = (!isHullIntact && hasEnoughCrew && isBoxProvisions)
+            || (isHullIntact && hasEnoughCrew1 && isBoxProvisions1 && isWeatherSafe)
 
     println(canDeparture)
 
