fb648923a6aa8932d41d50ec8242e4e10e32b0f1
Савин А <savin.alexey78@gmail.com>
2026-08-04 22:20:56 +0300

    KS-4-5 can departure final

--- src/main/kotlin/ks_4_5/ks_4_5.kt
+++ src/main/kotlin/ks_4_5/ks_4_5.kt
@@ -5,7 +5,7 @@ fun main() {
     val hull = readln().toBoolean()
     val isHullIntact = hull == true
     val crew = readln().toInt()
-    val hasEnoughCrew = crew in ENOUGH_CREW_LOWER..ENOUGH_CREW_UPPER
+    val hasEnoughCrew = crew >= ENOUGH_CREW_LOWER && crew <= ENOUGH_CREW_UPPER
     val hasEnoughCrew1 = crew == ENOUGH_CREW_UPPER
     val provisions = readln().toInt()
     val isBoxProvisions = provisions > BOX_PROVISION
