fcc238d7d1f98cc62d24aed59039add568c6fab2
Савин А <savin.alexey78@gmail.com>
2026-08-08 22:20:21 +0300

    KS-5-1 Bot detection authentication 2

--- src/main/kotlin/ks_5_1/ks_5_1.kt
+++ src/main/kotlin/ks_5_1/ks_5_1.kt
@@ -2,12 +2,12 @@ package org.example.ks_5_1
 
 fun main() {
 
-    val firstNumber = readln().toInt()
-    val secondNumber = readln().toInt()
+    val firstNumber = 33
+    val secondNumber = 56
     val amoundAddition = firstNumber + secondNumber
     println("Проверка на бота. Сложите два числа: $firstNumber + $secondNumber")
     val resultAddition = readln().toInt()
     if (resultAddition == amoundAddition) println("Добро пожаловать!")
-    else println("Доступ запрещён.")
+    else println("Доступ запрещен.")
 
 }
