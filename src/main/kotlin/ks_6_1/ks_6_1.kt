f55d337b151f35ee55f727f6e40e0c75ccdd07b5
AlexeySavin78 <savin.alexey78@gmail.com>
2026-08-19 10:30:34 +0300

    KS-6-1 Authorization 3

--- src/main/kotlin/ks_6_1/ks_6_1.kt
+++ src/main/kotlin/ks_6_1/ks_6_1.kt
@@ -6,13 +6,15 @@ fun main() {
     val login = readln()
     println("Создайте пароль:")
     val password = readln()
-    println("Регистрация прошла успешно.Для авторизации введите логин и пароль:")
+    println("Регистрация прошла успешно.")
+    println("Для авторизации введите логин и пароль:")
     var loginUser = readln()
     var passwordUser = readln()
     while (loginUser != login || passwordUser != password) {
-        println("Неверный логин или пароль. Введите логин и пароль:")
+        println("Неверный логин или пароль.")
+        println("Введите логин и пароль:")
         loginUser = readln()
         passwordUser = readln()
     }
-    println("Авторизация прошла успешно")
+    println("Авторизация прошла успешно.")
 }
