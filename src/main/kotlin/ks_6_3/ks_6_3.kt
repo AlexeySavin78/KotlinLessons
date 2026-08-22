56302e846ae3b1f81e5537c867b65ed787e37eed
AlexeySavin78 <savin.alexey78@gmail.com>
2026-08-22 18:06:21 +0300

    KS-6-3 Countdown timer 7

--- src/main/kotlin/ks_6_3/ks_6_3.kt
+++ src/main/kotlin/ks_6_3/ks_6_3.kt
@@ -3,11 +3,11 @@ package org.example.ks_6_3
 fun main() {
 
     println("Таймер. Введите колличество секунд, которое нужно засечь:")
-    var secondsCount = readln().toInt()
+    var seconds = readln().toInt()
     do {
-        println("Осталось секунд: $secondsCount")
+        println("Осталось секунд: $seconds")
         Thread.sleep(1000)
-        secondsCount--
-    } while (secondsCount > 0)
+        seconds--
+    } while (seconds > 0)
     println("Время вышло")
 }
